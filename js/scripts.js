f8c225f5c256f497bb551756e85193f70bf0baa5
Wei Dai <wei@>
2019-11-01 11:19:34 -0700

    continue to do further research on project

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,23 @@ $(document).ready(function() {
   $("form#digit").submit(function(event) {
     event.preventDefault();
 
+    var int = 0;
+    var result = "";
+
     if (total === 1) {
-      $(".result").text("Beep!")
+      $(".result").text("Beep!");
     } else if (total === 2) {
-      $(".result").text("Boop!")
+      $(".result").text("Boop!");
     } else if (total === 3) {
-      $(".result").text("I'm sorry, Dave. I'm afraid I can't do that.")
+      $(".result").text("I'm sorry, Dave. I'm afraid I can't do that.");
     }
 
-    $(".result").show();
   });
+    $(".result").show();
+
+   // var tempInput = input;
+   // if(input < 4000){
+   //  for(i = 0; i < int.length; i++) {
+   //   while(tempInput >= baseTen[v]) {
+   //     result += romanNumerals[v];
+   //     tempInput -= baseTen[v];
