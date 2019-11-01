200fb8e15406c94af619be55058875c4ef7cec2d
Wei Dai <wei@>
2019-11-01 09:26:51 -0700

    set up foundation of website, start researching

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("form.digit").submit(function(event){
+    event.preventDefault();
+
+    var number =
+
+  });
+});
+
+// "0"
+// "Beep!"
+// "Boop!"
+// "I'm sorry, Dave. I'm afraid I can't do that."
