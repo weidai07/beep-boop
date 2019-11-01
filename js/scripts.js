5e1525cb62d9c06e08396f0f830860a45f0b43d0
Wei Dai <wei@>
2019-11-01 12:07:23 -0700

    continue to do more research

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,27 @@ $(document).ready(function() {
   $("form#digit").submit(function(event) {
     event.preventDefault();
 
-    var int = 0;
+    // var none = 0;
+    // var one = 1;
+    // var two = 2;
+    // var three = 3;
+    var total = ("1", "2", "3");
     var result = "";
-
-    if (total === 1) {
-      $(".result").text("Beep!");
-    } else if (total === 2) {
-      $(".result").text("Boop!");
-    } else if (total === 3) {
-      $(".result").text("I'm sorry, Dave. I'm afraid I can't do that.");
+    //
+    if (total === "1") {
+      $(".result").append("Beep!");
+      // alert ("Beep!");
+    // } else if (total === 2) {
+    //   $(".result").text("Boop!");
+    // } else if (total === 3) {
+    //   $(".result").text("I'm sorry, Dave. I'm afraid I can't do that.");
     }
-
-  });
+    return result; 
     $(".result").show();
+  });
+});
 
+   //
    // var tempInput = input;
    // if(input < 4000){
    //  for(i = 0; i < int.length; i++) {
