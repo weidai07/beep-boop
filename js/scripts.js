f3353ccdf9237d0a0072eac2b5f05f3964f442bc
Student <example@example.com>
2019-11-22 17:22:29 -0800

    make adjustments to project

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,29 @@
-$(document).ready(function() {
-  $("form#digit").submit(function(event) {
-    event.preventDefault();
-
-    var userInput = $("#userInput1").val();
     var results = [];
 
     function beep(num) {
-      for (var i = 0; i <= num; i++) {
-        results[i] = i + " ";
-        var list = results[i].toString();
-       if (list.includes(3)) {
-         results[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
-       } else if (list.includes(2)) {
-         results[i] = "Boop!";
-       } else if (list.includes(1)) {
-         results[i] = "Beep!";
-       } else {
-         return results;
-       };
-     }};
+    for (var i = 0; i <= num; i++) {
+      results[i]=i.toString();
+     if (results[i].includes(3)) {
+       results[i]="I'm sorry, Dave. I'm afraid I can't do that.";
+     } else if (results[i].includes(2)) {
+       results[i]="Boop!";
+     } else if (results[i].includes(1)) {
+       results[i]="Beep!";
+     } else {
+       results.push(i);
+     }
+      return results;
+     };
+    };
 
-     beep();
-     $("#box").append(userInput);
+
+$(document).ready(function() {
+$("form#digit").submit(function(event) {
+  event.preventDefault();
+
+  var userInput = parseInt($("#userInput1").val());
+  beep(userInput);
+  $("#box").append();
 
   });
 });
